f89f261f6bc8eeeb65336755928a5a75471cab78
freewind <nowindlee@gmail.com>
2018-12-26 22:17:51 +0800

    demo

--- hello.ts
+++ hello.ts
@@ -1,3 +1,16 @@
-import {capitalize} from 'lodash';
+import * as puppeteer from 'puppeteer';
 
-console.log("Hello, " + capitalize("typescript") + "!");
+async function run() {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+
+  console.log('open github');
+  await page.goto("http://github.com");
+
+  console.log('save a screenshot');
+  await page.screenshot({path: 'screenshots/github.png'});
+
+  await browser.close();
+}
+
+run();
